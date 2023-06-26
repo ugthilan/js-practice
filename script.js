5efb73b57738bf2f2b0dc83d8995ff7d26e430c6
ugthilan <tsuyangodage@gmail.com>
2023-06-26 12:48:12 +0530

    switch statement

--- script.js
+++ script.js
@@ -1,52 +1,18 @@
-//if statements,comparison,logical operators
-
-
+  //if statements,comparison,logical operators
 const inA = document.getElementById('inA');
-const inB = document.getElementById('inB');
-const btn = document.getElementById('btn');
+//const inB = document.getElementById('inB');
+const btn = document.getElementById('btn'); 
 
-
-btn.addEventListener('click', function(e) {
-    if(inA.value > inB.value){
-       window.alert('A is greater than B');
-
-    }else if(inA.value == inB.value){
-        window.alert('A is equal to B');
+btn.addEventListener('click',function(){
+    switch (inA.value){
+        case '1':
+            alert('thilan');
+            break
+        case '2':
+            alert('ts');
+            break
+        default:
+            alert('no one');
+            break;      
     }
-    
-    else{
-        window.alert('B is greater than A');
-    }
-    
-    
-});
-
-
-let a = 10;
-let b = 20;
-let c = 10;
-
-if(a != b && a != c);{
-    console.log('A is not equal to b and C')
-}
-
-console.log( a>b && a>c)
-console.log( a>b || b>c)
-
-
-// function check odd numbers
-
-function checkOdd(num) {
-    if(num %2 == 0 ){
-        return false;
-    }
-    else{
-        return true;
-    }
-}
-
-if(checkOdd(10)){
-    console.log('Odd');
-}else{
-    console.log('Even');
-}
+})
